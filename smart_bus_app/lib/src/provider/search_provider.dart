61297fc2c20ee1c369ec7111f5ad60f3a8d8d5eb
홍순원 <sunwonsw95@naver.com>
2021-07-16 11:52:52 +0900

    불필요한 코드 제거

--- smart_bus_app/lib/src/provider/search_provider.dart
+++ smart_bus_app/lib/src/provider/search_provider.dart
@@ -5,46 +5,21 @@ import 'package:smart_bus_app/src/web_server.dart';
 enum Type{ BUS, STATION }
 
 class SearchProvider with ChangeNotifier{
-  /*
-  String _busNum = '';
-  String _departure = '';
-  String _destination = '';
-   */
   String _keyword;
   Type _type;
-
-  /*
-  String get busNum => _busNum;
-  String get departure => _departure;
-  String get destination => _destination;
-   */
 
   String get keyword => _keyword;
   Type get type => _type;
 
+  List<dynamic> _list = [];
+  List<dynamic> get list => _list;
+
   search(String keyword, Type type){
-    /*
-    switch(type){
-      case Type.BUS:
-        _busNum = keyword;
-        break;
-      case Type.STATION_DEPARTURE:
-        _departure = keyword;
-        break;
-      case Type.STATION_DESTINATION:
-        _destination = keyword;
-        break;
-    }
-     */
     _keyword = keyword;
     _type = type;
 
     notifyListeners();
   }
-
-
-  List<dynamic> _list = [];
-  List<dynamic> get list => _list;
 
   ///출발지 선택 화면 검색어 필터링
   void filtering() async {
@@ -56,21 +31,18 @@ class SearchProvider with ChangeNotifier{
     switch(_type){
       case Type.BUS:
         tmpList = await WebServer().getBusList(_keyword);
-        //리스트에 채워넣기
         tmpList.forEach((element) {
           _list.add(BusItem(element));
         });
         break;
       case Type.STATION:
         tmpList = await WebServer().getStationList(_keyword);
-        //리스트에 채워넣기
         tmpList.forEach((element) {
           _list.add(StationItem(element));
         });
         break;
     }
-
     notifyListeners();
   }
-
 }
+
